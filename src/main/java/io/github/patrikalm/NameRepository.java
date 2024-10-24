3240f4c6d6746bc73840e095a6e38116e871abe8
Patrik Alm <almpatrik@yahoo.se>
2024-10-24 13:57:56 +0200

    Added methods findByLastName and findByFirstName and updated setNames

--- src/main/java/io/github/patrikalm/NameRepository.java
+++ src/main/java/io/github/patrikalm/NameRepository.java
@@ -4,63 +4,115 @@ import java.util.Arrays;
 
 public class NameRepository {
 
-    private static String [] names = new String[0];
+    private static String[] names = {"Sven Svensson", "Sven Göransson", "Knut Knutsson", "Göran Svensson", "Sven Arnesson"};
 
+    public static void main(String[] args) {
 
-    public static int getSize() {
+        String [] localNames = Arrays.copyOf(findByLastName("Svensson"), findByLastName("Svensson").length);
 
-        return names.length;
-
-    }
-
-    public static void setNames( String [] newNames) {
-
-        names = newNames;
+        System.out.println(Arrays.toString(localNames));
 
     }
 
-    public static void clear() {
+        public static int getSize () {
 
-      names = new String[0];
-
-    }
-
-
-    public static String[] findAll() {
-
-        return names.clone();
-
-    }
-
-    public static String find(final String fullName) {
-
-        for (int i = 0; i < names.length; i++) {
-
-            if (names[i].equals(fullName)) {
-
-                return names[i];
-            }
+            return names.length;
 
         }
 
-        return null;
-    }
+        public static void setNames (String[]newNames){
 
-    public static boolean add(final String fullName) {
+            names = Arrays.copyOf(newNames, newNames.length);
 
-        if (find(fullName) != null) {
+        }
 
-            return false;
+        public static void clear () {
+
+            names = new String[0];
+
+        }
+
+
+        public static String[] findAll () {
+
+            return Arrays.copyOf(names, names.length);
+
+        }
+
+        public static String find ( final String fullName){
+
+            for (int i = 0; i < names.length; i++) {
+
+                if (names[i].equals(fullName)) {
+
+                    return names[i];
+                }
 
             }
-        else {
 
-            names = Arrays.copyOf(names, names.length + 1);
+            return null;
+        }
 
-            names [names.length-1] = fullName;
+        public static boolean add ( final String fullName){
 
-            return true;
+            if (find(fullName) != null) {
+
+                return false;
+
+            } else {
+
+                names = Arrays.copyOf(names, names.length + 1);
+
+                names[names.length - 1] = fullName;
+
+                return true;
 
             }
         }
-    }
+
+        public static String[] findByFirstName(final String firstName) {
+
+            String[] localNames = new String[0];
+
+            String localFirstName = null;
+
+            for (int i = 0; i < names.length; i++) {
+
+                localFirstName = names[i].split(" ")[0];
+
+                if (localFirstName.equals(firstName)) {
+
+                    localNames = Arrays.copyOf(localNames, localNames.length + 1);
+                    localNames[localNames.length - 1] = names[i];
+
+                    }
+                 }
+
+            return localNames;
+
+        }
+
+        public static String[] findByLastName ( final String lastName){
+
+            String[] localNames = new String[0];
+
+            String localLastName = null;
+
+            for (int i = 0; i < names.length; i++) {
+
+                localLastName = names[i].split(" ")[1];
+
+                if (localLastName.equals(lastName)) {
+
+                    localNames = Arrays.copyOf(localNames, localNames.length + 1);
+                    localNames[localNames.length - 1] = names[i];
+
+                    }
+                }
+
+            return localNames;
+
+        }
+
+
+}
